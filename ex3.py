45e0db80c3d0706bd3f7e8eea3484a42ce8ea486
michaelmuttiah <michael.muttiah@gmail.com>
2018-06-28 17:08:18 +0100

    Add files via upload

--- ex3.py
+++ ex3.py
@@ -1,11 +1,20 @@
-print "I will now count my chickenso:"
 
-print "Hens", 25 + 30 / 6
+print "I will now count my chickens:"
+
+print "Hens", 25+30/6 #I first thought this sum was wrong, as in calculating 25 + 30
+# =55 then dividng by 6, but is in fact 25 +(30/6)
+
 print "Roosters", 100 - 25 * 3 % 4
+#Same here so this is 100 - (25*3%4)
 
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4.0 + float(6)
+
+#To return a float for this value there are three ways, two are as above
+#Writing one number as a float i.e. 4.0 , writing float(n)
+#Or much easier in future is importing division as a function
+#from __future__ import division
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
---
+++ ex3practise1.py
@@ -0,0 +1,28 @@
+print "How many fingers am I holding up?"
+
+print "If I have 10 fingers of course."
+
+print "How about 2 thumbs up?"
+print "Fingers: ", 1+1
+
+print "What about if I am rocking out?"
+
+print "Fingers: ", 2 + 2 + 2
+
+print "Now I'm giving you the finger!"
+
+print "Fingers: ", 6 - 2 - 2
+
+print "No just one hand please"
+
+print "Fingers: ", 2/2
+
+print "Now someone has seen me, and he is giving me both fingers"
+
+print "Fingers: ", 1*3
+
+print "I hold my hands up in surrender and he punches me in the face"
+
+print "Fingers: ", 5*2-0
+
+print "BEWARE THE FINGER GAME!"
