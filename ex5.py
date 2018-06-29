9a6cef30d0239fec565644bba6176c682fb9e9b0
michaelmuttiah <michael.muttiah@gmail.com>
2018-06-29 14:27:36 +0100

    Add files via upload

--- ex5.py
+++ ex5.py
@@ -1,18 +1,20 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+human_name = 'Zed A. Shaw'
+human_age = 35 # not a lie
+human_height = 74 # inches
+human_height_cm = human_height * 2.54
+human_weight = 180 # lbs
+human_weight_kg = human_weight / 2.2
+human_eyes = 'Blue'
+human_teeth = 'White'
+human_hair = 'Brown'
 
-print "Lets talks about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy" % my_weight
+print "Lets talks about %s." % human_name
+print "He's %d inches tall or %d cms tall ." % (human_height, human_height_cm)
+print "He's %d pounds or %d kgs heavy" % (human_weight, human_weight_kg)
 print "Actually that's not too heavy"
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee" % my_teeth
+print "He's got %s eyes and %s hair." % (human_eyes, human_hair)
+print "His teeth are usually %s depending on the coffee" % human_teeth
 
 #this line is tricky try to get it right exactly
 print "If I add %d, %d, and %d I get %d." % (
-my_age, my_height, my_weight, my_age + my_height + my_weight)
+human_age, human_height, human_weight, human_age + human_height + human_weight)
